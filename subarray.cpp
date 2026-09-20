872ef1b2f7439d06093b06ae5c04f68fcf6ac90c
Abu Rayhan Kobir <aburayhankobir013@gmail.com>
2026-09-21 05:06:07 +0600

    subarray problem solve

---
+++ subarray.cpp
@@ -0,0 +1,42 @@
+/*
+
+(1) => Subarray হলো একটি array-এর এমন একটি অংশ, যেখানে element-গুলো অবশ্যই পরপর (contiguous) থাকতে হবে। 
+(2) => Subarray = Continuous part of an array
+
+array = [1, 2, 3]
+array size = n
+total sub array = (n) * (n + 1) / 2
+{
+total sub array:
+(1), (2), (3)
+(1, 2), (2, 3)
+(1, 2, 3)
+}
+
+not sub array:
+(1, 3) => no contiguous part of an array, 2 missing
+*/
+
+# include <iostream>
+# include <vector>
+
+void totalSubArray (std :: vector <int>& numbers);
+
+int main () {
+  std :: vector <int> numbers = {1, 2, 3};
+  totalSubArray (numbers);
+  return 0;
+}
+
+void totalSubArray (std :: vector <int>& numbers) {
+  for (int start = 0; start < numbers.size (); start++) {
+    for (int end = start; end < numbers.size (); end++) {
+      for (int count = 0; count <= end; count++) {
+        std :: cout << numbers.at (count);
+      }
+      std :: cout << " ";
+    }
+    std :: cout << std :: endl;
+  }
+  return;
+}
