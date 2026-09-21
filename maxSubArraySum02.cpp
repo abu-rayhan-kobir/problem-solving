6a8cf936f6a222868e2ad05dc4c2f23031b0d919
Abu Rayhan Kobir <aburayhankobir013@gmail.com>
2026-09-21 06:00:38 +0600

    Add second method to find out the maximum sub array sum with O (n2) time complexity

---
+++ maxSubArraySum02.cpp
@@ -0,0 +1,33 @@
+# include <iostream>
+# include <vector>
+
+int maxValue (int number1, int number2) {
+  if (number1 > number2) {
+    return number1;
+  } else if (number2 > number1) {
+    return number2;
+  } else {
+    return number1;
+  }
+}
+
+int maxSubArraySum (std :: vector <int>& numbers);
+
+int main () {
+  std :: vector <int> numbers = {1, 2, 3, 4};
+  int maxSum = maxSubArraySum (numbers);
+  std :: cout << "Maximum sum: " << maxSum << std :: endl;
+  return 0;
+}
+
+int maxSubArraySum (std :: vector <int>& numbers) {
+  int maxSum = INT_MIN;
+  for (int start = 0; start < numbers.size (); start++) {
+    int currentSum = 0;
+    for (int end = start; end < numbers.size (); end++) {
+      currentSum += numbers.at (end);
+    }
+    maxSum = maxValue (currentSum, maxSum);
+  }
+  return maxSum;
+}
